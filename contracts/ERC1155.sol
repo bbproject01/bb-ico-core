fcd0e4f0e219c92acb6a4b90ee98ea87fdb1eb6c
Augusto Sanchez Julian <julian.a@dicio.ai>
2023-05-23 10:55:57 -0600

    Se agregan funcionalidades del ERC20

--- contracts/ERC1155.sol
+++ contracts/ERC1155.sol
@@ -6,12 +6,17 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract BNBEnviroment is ERC1155 {
     // Constantes
-    uint256 public constant BNB_TOKEN = 1;
+    uint256 public constant BNB_TOKEN = 0;
 
     // Variables
     using Counters for Counters.Counter;
-    Counters.Counter private _totalTypes;
+    Counters.Counter private _totalFNFTs;
     mapping (uint256 => string) _listUris;
+    // Variables para ERC20
+    string private _nameToken;
+    string private _symbolToken;
+    uint8 private _decimalsToken;
+    uint256 private _totalSupplyTokens;
 
     // Eventos
     event CreateToken( address indexed  owner, uint256 indexed _id, uint256 _amount, bytes _data);
@@ -20,33 +25,70 @@ contract BNBEnviroment is ERC1155 {
     // Modificadores
 
     constructor() ERC1155("") {
-        _totalTypes.increment();
+        // ERC20
+        _nameToken = "BNB Token";
+        _symbolToken = "BNB";
+        _decimalsToken = 18;
+        _totalSupplyTokens = 10**_decimalsToken;
+        _mint(msg.sender, BNB_TOKEN, _totalSupplyTokens, "");
+
+        // ERC721
         _listUris[BNB_TOKEN] = "https://bafkreigaodcf2yaiubhvh7jko57zeruwr6k2zcej6ftv4aglg7mtoosz7u.ipfs.nftstorage.link/";
-        _mint(msg.sender, BNB_TOKEN, 10**18, "");
+    }
+
+    // Funciones ERC20
+
+    function nameToken() public view returns(string memory){
+        return _nameToken;
+    }
+
+    function symbolToken() public view returns(string memory){
+        return _symbolToken;
+    }
+
+    function decimalsToken() public view returns(uint8){
+        return _decimalsToken;
+    }
+
+    function totalSupplyToken() public vire returns(uint256){
+        return _totalSupplyTokens;
+    }
+
+    function balanceOfToken() public view returns(uint256){
+        return balanceOf(msg.sender, BNB_TOKEN);
+    }
+
+    function transferToken(address _to, uint256 _amount ) public returns(bool){
+        return safeTransferFrom(msg.sender, _to, BNB_TOKEN, _amount, "");
+    }
+
+    // Funciones ERC721
+
+    function balanceOfFNFT(address _owner) external view returns (uint256){
+        
     }
 
     function uri(uint256 _tokenid) override public view returns (string memory) {
         return _listUris[_tokenid];
     }
 
-    function createFNFT(uint256 _amount, string memory _urlURI) public returns(uint256){
-        _totalTypes.increment();
-        uint256 _id = _totalTypes.current();
+    function createFNFT(string memory _urlURI) public returns(uint256){
+        _totalFNFTs.increment();
+        uint256 _id = _totalFNFTs.current();
         _listUris[_id] = _urlURI;
-        _mint(msg.sender, _id, _amount, "");
-        emit CreateToken( msg.sender, _id, _amount, "");
+        _mint(msg.sender, _id, 1, "");
+        emit CreateToken( msg.sender, _id, 1, "");
         return _id;
     }
 
-    function createFNFTBatch(uint256[] memory _amounts, string[] memory _urlURI) public returns( uint256[] memory){
-        require(_urlURI.length == _amounts.length, "Los arrays deben tener la misma longitud");
-        uint256[] memory _ids = new uint[](_amounts.length);
-        for (uint i = 0; i < _amounts.length; i++) {
-            _totalTypes.increment();
-            uint256 _id = _totalTypes.current();
+    function createFNFTBatch(string[] memory _urlURI) public returns( uint256[] memory){
+        uint256[] memory _ids = new uint[](_urlURI.length);
+        for (uint i = 0; i < _urlURI.length; i++) {
+            _totalFNFTs.increment();
+            uint256 _id = _totalFNFTs.current();
             _listUris[_id] = _urlURI[i];
-            _mint(msg.sender, _id, _amounts[i], "");
-            emit CreateToken( msg.sender, _id, _amounts[i], "");
+            _mint(msg.sender, _id, 1, "");
+            emit CreateToken( msg.sender, _id, 1, "");
             _ids[i] = _id;
         }
         return _ids;
